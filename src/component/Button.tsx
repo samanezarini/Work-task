4d9da5ded3fca071b0c872c11996f0eb734ca660
samane zarini <samane_zarini@yahoo.com>
2024-11-28 23:17:27 +0330

    create button

---
+++ src/component/Button.tsx
@@ -0,0 +1,22 @@
+function Button() {
+    return (
+      <>
+        <div className="flex flex-col justify-center items-center p-10">
+          <h1 className="text-2xl">Select an option :</h1>
+          <div className="flex space-x-4 shadow-md p-16">
+            <button className="bg-blue-500 w-28 text-white px-4 py-2 rounded-lg hover:bg-blue-400">
+              Blue
+            </button>
+            <button className="bg-red-500 w-28 text-white px-4 py-2 rounded-lg hover:bg-red-400">
+              Red
+            </button>
+            <button className="bg-yellow-500 w-28 text-white px-4 py-2 rounded-lg hover:bg-yellow-400">
+              Yellow
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default Button;
