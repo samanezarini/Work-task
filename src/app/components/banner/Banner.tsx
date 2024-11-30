471971b51b442b956c85a849210ddb84170eea28
samane zarini <samane_zarini@yahoo.com>
2024-11-30 18:48:45 +0330

    edited banner

--- src/app/components/banner/Banner.tsx
+++ src/app/components/banner/Banner.tsx
@@ -7,30 +7,20 @@ function Banner() {
     return (
         <div className='flex justify-center items-center mt-10'>
             <div className="w-[70rem] h-[30rem] bg-blue-700 text-white py-16 px-4 text-center">
-                <div className="max-w-4xl mx-auto">
+                <div className="max-w-4xl mx-auto mt-52">
                     <h1 className="text-4xl font-bold mb-4">هر چیز که در جستن آنی ... آنی</h1>
-                    <button className="bg-orange-500 text-white px-6 py-3 rounded-2xl text-lg hover:bg-orange-600 transition">
+                    <button className="text-black bg-orange-500 px-3 py-1 rounded-2xl text-lg hover:bg-orange-600 transition">
                         کانون تبلیغات آریا
                     </button>
                 </div>
-
-                <div className="container grid grid-cols-2 md:grid-cols-4 gap-4 mt-10">
-                    <div className="bg-white text-gray-950 py-2 rounded-lg flex">چند رسانه‌ای</div>
-                    <div className="bg-white text-gray-950 py-2 rounded-lg flex">طراحی تخصصی</div>
-                    <div className="bg-white text-gray-950 py-2 rounded-lg flex">هدایای تبلیغاتی</div>
-                    <div className="bg-white text-gray-950 py-2 rounded-lg flex">
-                        <p>انواع چاپ</p>
-                        <LuNewspaper />
-                    </div>
-                </div>
                 
-                <div className="bg-blue-800 py-4">
+                <div className="flex justify-center w-[55rem] bg-gray-100 ml-20 py-4 mt-16 shadow-md">
                     <div className="container mx-auto flex justify-center space-x-8">
                         {["چند رسانه ای", "طراحی تخصصی", "هدایای تبلیغاتی", "انواع چاپ"].map(
                             (item) => (
                                 <button
                                     key={item}
-                                    className="text-white font-semibold hover:bg-blue-600 px-4 py-2 rounded-lg transition"
+                                    className="text-black font-semibold hover:bg-blue-600 px-4 py-2 rounded-lg transition"
                                 >
                                     {item}
                                 </button>
@@ -40,8 +30,6 @@ function Banner() {
                 </div>
             </div>
         </div>
-
-
     );
 }
 
